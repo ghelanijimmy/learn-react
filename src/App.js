7b182b9e5d4417f6dde5d95c342fe17c81e3afed
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2017-10-29 17:54:16 -0400

    simple component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           Let{"'"}s build someting cool!
         </p>
+        <Person />
       </div>
     );
   }
